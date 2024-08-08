2be0fb59f69926bcfd1a7f6eefee9d96e1afeedf
yiming huang <study0608@126.com>
2024-08-08 20:11:17 +0800

    8/8 merge 1

--- T_380026.cpp
+++ T_380026.cpp
@@ -7,12 +7,12 @@ void _Search(int point,int step,int ans)
 {
     //cout<<point<<step<<ans<<endl;
     if(step>d) {res=max(res,ans);return;}
-    else for(int i=0;i<arr[point].size();i++) if(!vis[arr[point][i]]) {vis[arr[point][i]]=true;_Search(arr[point][i],step+1,ans+1);vis[arr[point][i]]=false;cout<<point<<step<<ans<<"to\n";}
+    else for(int i=0;i<arr[point].size();i++) if(!vis[arr[point][i]]) {vis[arr[point][i]]=true;_Search(arr[point][i],step+1,ans+1);vis[arr[point][i]]=false;/*cout<<point<<step<<ans<<"to\n";*/}
 }
 signed main()
 {
 	cin>>n>>d;
-    for(int i=1;i<=n-1;i++) {int from,to;cin>>from>>to;arr[from].push_back(to);/*arr[to].push_back(from);*/}
+    for(int i=1;i<=n-1;i++) {int from,to;cin>>from>>to;arr[from].push_back(to);arr[to].push_back(from);}
     _Search(1,0,0);
     cout<<res;
 	return 0;
