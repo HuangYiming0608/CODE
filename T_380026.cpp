e98279347dae6126735d8e9808232fdd16fc4104
yiming huang <study0608@126.com>
2024-08-09 17:03:31 +0800

    8/9 MERGE 2

--- T_380026.cpp
+++ T_380026.cpp
@@ -3,19 +3,15 @@
 using namespace std;
 int res=0,n,d,vis[N];
 vector <int> arr[N];
-void _Search(int point,int step,int ans)
-{
-    //cout<<point<<step<<" "<<ans<<endl;
-    if(step>=d) {res+=ans;return;}
-    for(int i=0;i<arr[point].size();i++) if(!vis[arr[point][i]]) {vis[arr[point][i]]=true;_Search(arr[point][i],step+1,ans+1);vis[arr[point][i]]=false;/*cout<<point<<step<<ans<<"to\n";*/}
-}
-signed main()
+void _Search(int point,int step)
+    {vis[point]=true;
+    for(int i=0;i<arr[point].size();i++) if(!vis[arr[point][i]]&&step<d) {res++;vis[arr[point][i]]=true;_Search(arr[point][i],step+1);}}
+int main()
 {
 	cin>>n>>d;
     for(int i=1;i<=n-1;i++) {int from,to;cin>>from>>to;arr[from].push_back(to);arr[to].push_back(from);}
-    _Search(1,0,0);
+    _Search(1,0);
     cout<<res;
 	return 0;
 }
 // I AK IOI
-// 垃圾 CCF
