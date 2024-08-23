d9cb13c95f86bb337cf2aae34ac8c04920aab775
yiming huang <study0608@126.com>
2024-08-23 15:08:22 +0800

    8/23 merge 1

--- ABC367/C.cpp
+++ ABC367/C.cpp
@@ -8,5 +8,5 @@ int main()
     int n,k,sum=0;
     cin>>n>>k;
     for(int i=1;i<=n;i++) cin>>r[i];
-    for(int i=1;i<=n;i++) for(int j=1;j<=r[i];j++) ;
+    for(int i=1;i<=n;i++) for(int j=1;j<=r[i];j++);
 }
